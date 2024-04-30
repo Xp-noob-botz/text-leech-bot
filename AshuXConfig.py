fe396b5c592da7d9b1a4765788443fe8e278eebe
akpandawep <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-05-01 03:27:24 +0530

    Last Of Tuday Fix Donate Txt

--- AshuXConfig.py
+++ AshuXConfig.py
@@ -18,6 +18,11 @@ BUY_PRIME_PICS = (environ.get('BUY_PRIME_PICS', 'https://graph.org/file/867866ae
 
 USE = (environ.get('USE', 'https://graph.org/file/e014588ed2c1b2bf5b82a.jpg https://graph.org/file/e014588ed2c1b2bf5b82a.jpg')).split()
 
+DONATE_TXT = """<b>𝗧𝗵𝗮𝗻𝗸𝘀 𝗙𝗼𝗿 𝗦𝗵𝗼𝘄𝗶𝗻𝗴 𝗜𝗻𝘁𝗲𝗿𝗲𝘀𝘁 𝗜𝗻 𝗗𝗼𝗻𝗮𝘁𝗶𝗼𝗻! ❤️</b>
+
+𝐈𝐟 𝐘𝐨𝐮 𝐋𝐢𝐤𝐞 𝐌𝐲 𝐁𝐨𝐭𝐬 & 𝐏𝐫𝐨𝐣𝐞𝐜𝐭𝐬, 𝐘𝐨𝐮 𝐂𝐚𝐧 🎁 𝐃𝐨𝐧𝐚𝐭𝐞 𝐌𝐞 𝐀𝐧𝐲 𝐀𝐦𝐨𝐮𝐧𝐭 𝐅𝐫𝐨𝐦 𝟏𝟎𝐌 𝐑𝐬 😁 𝐔𝐩𝐭𝐨 𝐘𝐨𝐮𝐫 𝐂𝐡𝐨𝐢𝐜𝐞.
+
+<b>🛍 𝗨𝗣𝗜 𝗜𝗗:</b> <code>PandaWep@ybl</code>"""
 
 USE_TXT = """
 <b><i>
